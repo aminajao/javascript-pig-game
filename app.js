78e09b607aa7e12892078a81a2355545578368b3
Amin Ajao <aminajao96@gmail.com>
2019-08-16 18:34:20 +0100

    all game functionalities done

--- app.js
+++ app.js
@@ -13,10 +13,72 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // generate random number from 1-6
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    //display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+
+    //updating the round score if role number is not equal to one(1)
+    if(dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }  else {
+        //next player
+        nextPlayer()
+    }
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //check if player won the game
+    if(scores[activePlayer] >= 200) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!';
+        document.querySelector('.dice').style.display = 'none';
+    }  else{
+        //next player
+        nextPlayer();
+    }
+
+    //nextplayer
+    // nextPlayer();
+
+ 
+})
+
+function nextPlayer() {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+}
